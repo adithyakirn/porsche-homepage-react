682b07c1e97d02355040c6f6cb8cda5c1c269551
Adithya kiran C B <adithyakiran021@gmail.com>
2025-07-10 16:32:17 +0530

    Finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./styles/page.css";
-import "./styles/media.css";
 import Header from "./Header/header";
 import Hero from "./Hero section/Hero";
 import Cards from "./FeatureCardsSection/Cards";
